f0e83041ebfc5435babb1c2d60c7d6646a7dcfc8
Daria Lepikhova <d.lepikhova@postgrespro.ru>
2020-11-09 15:03:50 +0500

    Fixed security CVE_2020_14350. Added tests

--- shared_ispell--1.1.0.sql
+++ shared_ispell--1.1.0.sql
@@ -1,34 +1,34 @@
-CREATE OR REPLACE FUNCTION shared_ispell_init(internal)
+CREATE FUNCTION shared_ispell_init(internal)
 	RETURNS internal
 	AS 'MODULE_PATHNAME', 'dispell_init'
 	LANGUAGE C IMMUTABLE;
 
-CREATE OR REPLACE FUNCTION shared_ispell_lexize(internal,internal,internal,internal)
+CREATE FUNCTION shared_ispell_lexize(internal,internal,internal,internal)
 	RETURNS internal
 	AS 'MODULE_PATHNAME', 'dispell_lexize'
 	LANGUAGE C IMMUTABLE;
 
-CREATE OR REPLACE FUNCTION shared_ispell_reset()
+CREATE FUNCTION shared_ispell_reset()
 	RETURNS void
 	AS 'MODULE_PATHNAME', 'dispell_reset'
 	LANGUAGE C IMMUTABLE;
 
-CREATE OR REPLACE FUNCTION shared_ispell_mem_used()
+CREATE FUNCTION shared_ispell_mem_used()
 	RETURNS integer
 	AS 'MODULE_PATHNAME', 'dispell_mem_used'
 	LANGUAGE C IMMUTABLE;
 
-CREATE OR REPLACE FUNCTION shared_ispell_mem_available()
+CREATE FUNCTION shared_ispell_mem_available()
 	RETURNS integer
 	AS 'MODULE_PATHNAME', 'dispell_mem_available'
 	LANGUAGE C IMMUTABLE;
 
-CREATE OR REPLACE FUNCTION shared_ispell_dicts( OUT dict_name VARCHAR, OUT affix_name VARCHAR, OUT words INT, OUT affixes INT, OUT bytes INT)
+CREATE FUNCTION shared_ispell_dicts( OUT dict_name VARCHAR, OUT affix_name VARCHAR, OUT words INT, OUT affixes INT, OUT bytes INT)
     RETURNS SETOF record
     AS 'MODULE_PATHNAME', 'dispell_list_dicts'
     LANGUAGE C IMMUTABLE;
 
-CREATE OR REPLACE FUNCTION shared_ispell_stoplists( OUT stop_name VARCHAR, OUT words INT, OUT bytes INT)
+CREATE FUNCTION shared_ispell_stoplists( OUT stop_name VARCHAR, OUT words INT, OUT bytes INT)
     RETURNS SETOF record
     AS 'MODULE_PATHNAME', 'dispell_list_stoplists'
     LANGUAGE C IMMUTABLE;
